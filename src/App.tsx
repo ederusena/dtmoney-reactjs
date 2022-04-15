481eff59179ef6baa1afc83a9f44c248011faf5b
Eder Sena <eder.sena@live.com>
2022-04-15 18:09:11 -0300

    ✅: adding Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #8257e6;
+`;
+
 export function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <Title>Hello</Title>
     </div>
   );
 }
