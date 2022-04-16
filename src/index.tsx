1d1c0f2cf1509def4e9f45f44d8ff5912dc42793
Eder Sena <eder.sena@live.com>
2022-04-16 18:50:23 -0300

    ✅: adding Configurando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from "miragejs"
+
+// createServer mirageJS
+createServer({
+  routes() {
+    this.namespace = "api"
+    this.get("/transctions", () => {
+      return [
+        {
+          id: 1,
+          title: 'Transction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          date: new Date()
+        },
+      ]
+    })
+  }
+})
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
