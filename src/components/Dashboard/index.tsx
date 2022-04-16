606facca9b449b8dba6632386ac280a24828d089
Eder Sena <eder.sena@live.com>
2022-04-16 18:26:09 -0300

    ✅: adding Transaction Table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
-import Summary from "../Summary/summary";
+import Summary from "../Summary";
+import { TransactionTable } from "../TransactionsTable";
 import { Container } from "./dashboard.styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     );
 }
